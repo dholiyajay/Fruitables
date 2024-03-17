27571045f2b61a4f1752825c299349dd64a47476
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-03-17 12:27:25 +0530

    Exam_2_35Marks

--- src/user/admin/container/Shop/Shop.js
+++ src/user/admin/container/Shop/Shop.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useCallback, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Shop = () => {
     const [fruits, setFruits] = useState([])
     const [fruitName, setFruitName] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
     const fruitData = async () => {
         const response = await fetch("http://localhost:8000/fruits")
         const data = await response.json()
@@ -19,9 +21,16 @@ const Shop = () => {
 
 
 
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value)
+
+    }
+
+    
+
     return (
         <div>
-            {/* Single Page Header start */}
             <div className="container-fluid page-header py-5">
                 <h1 className="text-center text-white display-6">Shop</h1>
                 <ol className="breadcrumb justify-content-center mb-0">
@@ -30,9 +39,6 @@ const Shop = () => {
                     <li className="breadcrumb-item active text-white">Shop</li>
                 </ol>
             </div>
-            {/* Single Page Header End */}
-
-            {/* Fruits Shop Start*/}
             <div className="container-fluid fruite py-5">
                 <div className="container py-5">
                     <h1 className="mb-4">Fresh fruits shop</h1>
@@ -41,7 +47,7 @@ const Shop = () => {
                             <div className="row g-4">
                                 <div className="col-xl-3">
                                     <div className="input-group w-100 mx-auto d-flex">
-                                        <input type="search" className="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1" />
+                                        <input type="text" className="form-control p-3" onChange={handleSearch} placeholder="keywords" aria-describedby="search-icon-1" />
                                         <span id="search-icon-1" className="input-group-text p-3"><i className="fa fa-search" /></span>
                                     </div>
                                 </div>
@@ -81,70 +87,28 @@ const Shop = () => {
                                                     })
                                                 }
 
-                                                {/* 
-                                                <ul className="list-unstyled fruite-categorie">
-                                                    <li>
-                                                        <div className="d-flex justify-content-between fruite-name">
-                                                            <a href="#"><i className="fas fa-apple-alt me-2" />Apples</a>
-                                                            <span>({fruits.filter(fruit => fruit.name === "Apple").length})</span>
-                                                        </div>
-                                                    </li>
-                                                    <li>
-                                                        <div className="d-flex justify-content-between fruite-name">
-                                                            <a href="#"><i className="fas fa-apple-alt me-2" />Oranges</a>
-                                                            <span>({fruits.filter(fruit => fruit.name === "Orange").length})</span>
-                                                        </div>
-                                                    </li>
-                                                    <li>
-                                                        <div className="d-flex justify-content-between fruite-name">
-                                                            <a href="#"><i className="fas fa-apple-alt me-2" />Strawbery</a>
-                                                            <span>({fruits.filter(fruit => fruit.name === "Strawberry").length})</span>
-                                                        </div>
-                                                    </li>
-                                                    <li>
-                                                        <div className="d-flex justify-content-between fruite-name">
-                                                            <a href="#"><i className="fas fa-apple-alt me-2" />Banana</a>
-                                                            <span>({fruits.filter(fruit => fruit.name === 'Banana').length})</span>
-                                                        </div>
-                                                    </li>
-                                                    <li>
-                                                        <div className="d-flex justify-content-between fruite-name">
-                                                            <a href="#"><i className="fas fa-apple-alt me-2" />Grapes</a>
-                                                            <span>({fruits.filter(fruit => fruit.name === 'Grapes').length})</span>
-                                                        </div>
-                                                    </li>
-                                                </ul> */}
+
                                             </div>
                                         </div>
                                         <div className="col-lg-12">
                                             <div className="mb-3">
                                                 <h4 className="mb-2">Price</h4>
-                                                <input type="range" className="form-range w-100" id="rangeInput" name="rangeInput" min={0} max={500} defaultValue={0} oninput="amount.value=rangeInput.value" />
-                                                <output id="amount" name="amount" min-velue={0} max-value={500} htmlFor="rangeInput">0</output>
+                                                <input type="range" className="form-range w-100" id="rangeInput" name="rangeInput" min={1} max={8} onChange={(e) => setFruits(fruits.filter(fruit => fruit.price <= e.target.value))} oninput="amount.value=rangeInput.value" />
+                                                <output id="amount" name="amount" min-velue={1} max-value={8} htmlFor="rangeInput">0</output>
                                             </div>
                                         </div>
                                         <div className="col-lg-12">
                                             <div className="mb-3">
                                                 <h4>Additional</h4>
                                                 <div className="mb-2">
-                                                    <input type="radio" className="me-2" id="Categories-1" name="Categories-1" defaultValue="Beverages" />
+                                                {/* onClick={() => setFruits(fruits.filter(fruit => fruit.type === "Organic"))} */}
+                                                    <input type="radio"  className="me-2" id="Categories-1" name="Categories-1" defaultValue="Beverages" />
                                                     <label htmlFor="Categories-1"> Organic</label>
                                                 </div>
                                                 <div className="mb-2">
+                                                {/* onClick={() => setFruits(fruits.filter(fruit => fruit.type === "Fresh"))}  */}
                                                     <input type="radio" className="me-2" id="Categories-2" name="Categories-1" defaultValue="Beverages" />
                                                     <label htmlFor="Categories-2"> Fresh</label>
-                                                </div>
-                                                <div className="mb-2">
-                                                    <input type="radio" className="me-2" id="Categories-3" name="Categories-1" defaultValue="Beverages" />
-                                                    <label htmlFor="Categories-3"> Sales</label>
-                                                </div>
-                                                <div className="mb-2">
-                                                    <input type="radio" className="me-2" id="Categories-4" name="Categories-1" defaultValue="Beverages" />
-                                                    <label htmlFor="Categories-4"> Discount</label>
-                                                </div>
-                                                <div className="mb-2">
-                                                    <input type="radio" className="me-2" id="Categories-5" name="Categories-1" defaultValue="Beverages" />
-                                                    <label htmlFor="Categories-5"> Expired</label>
                                                 </div>
                                             </div>
                                         </div>
@@ -266,7 +230,6 @@ const Shop = () => {
                     </div>
                 </div>
             </div>
-            {/* Fruits Shop End*/}
         </div >
 
     );
